8aa7ff36e2f7be88c365e1302bd5ba009d449183
kingbrthr <rehmankingg143@gmail.com>
2025-10-16 16:53:12 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || ''
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~gUYDFTLJ#r9yMJcriLh7Yd955v6jEJIgL8GzZwH_3tETqGWQKq9c'
 };
